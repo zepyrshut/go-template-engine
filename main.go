65a0562da5632bf760b1774f4edcf570a3ab43ba
Pedro Pérez <hola@pedroperez.dev>
2024-06-20 22:51:13 +0200

    adding data to template

--- main.go
+++ main.go
@@ -6,15 +6,29 @@ import (
 	"path/filepath"
 )
 
-func renderTemplate(w http.ResponseWriter, templateName string) {
+type TemplateData struct {
+	Title string
+	Name  string
+}
+
+// renderTemplate se encarga de obtener la ruta de la plantilla, hacer el
+// procesamiento y su ejecución.
+
+func renderTemplate(w http.ResponseWriter, templateName string, data TemplateData) {
 	path := filepath.Join("templates", templateName)
 	tmpl, _ := template.ParseFiles(path)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, data)
 }
 
 func main() {
+
+	templateData := TemplateData{
+		Title: "Hola Mundo",
+		Name:  "Gopher",
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "index.html")
+		renderTemplate(w, "index.html", templateData)
 	})
 
 	http.ListenAndServe(":8080", nil)
